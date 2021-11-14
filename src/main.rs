080f3b5689d67c8393990bdd25c2e6a69290fff4
Simon Prickett <simon@crudworks.org>
2021-11-14 16:59:01 +0000

    Work in progress, got basic structure working.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::{process, thread, time};
+use ctrlc;
+
+fn allLightsOff() {
+    // TODO turn all the lights off...
+    println!("All lights off");
+}
+
 fn main() {
-    println!("Hello, world!");
+    ctrlc::set_handler(move || {
+        allLightsOff();
+        process::exit(0);
+    });
+
+    allLightsOff();
+
+    loop {
+        // Red
+        println!("Red");
+        thread::sleep(time::Duration::from_secs(3));
+
+        // Red and Yellow
+        println!("Red and Yellow");
+        thread::sleep(time::Duration::from_secs(1));
+
+        // Green
+        println!("Green");
+        thread::sleep(time::Duration::from_secs(5));
+
+        // Yellow
+        println!("Yellow");
+        thread::sleep(time::Duration::from_secs(2));
+
+        // Yellow off
+        println!("Yellow off");
+    }
 }
