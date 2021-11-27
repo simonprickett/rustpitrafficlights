39e9ba4b46ea70c25774ac6105913313b00e764f
Simon Prickett <simon@crudworks.org>
2021-11-27 21:10:35 +0000

    Fixed ordering of lights, changed ctrl-c handling.

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,50 @@
-use std::{process, thread, time};
+use std::{thread, time};
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
 use ctrlc;
 use rust_gpiozero::LED;
 
 fn main() {
+    let keep_going = Arc::new(AtomicBool::new(true));
+    let kg = keep_going.clone();
+
     let red = LED::new(9);
     let amber = LED::new(10);
     let green = LED::new(11);
 
-    //fn all_lights_off() {
-    //    RED.off();
-    //    AMBER.off();
-    //    GREEN.off();
-    //}
+    ctrlc::set_handler(move || {
+       kg.store(false, Ordering::SeqCst);
+    }).unwrap();
 
     red.off();
     amber.off();
     green.off();
 
-    ctrlc::set_handler(move || {
-        //all_lights_off();
-        process::exit(0);
-    }).unwrap();
-
-    //all_lights_off();
-
-    loop {
+    while keep_going.load(Ordering::SeqCst) {
         // Red
-        println!("Red");
         red.on();
         thread::sleep(time::Duration::from_secs(3));
 
         // Red and Yellow
-        println!("Red and Yellow");
         amber.on();
         thread::sleep(time::Duration::from_secs(1));
 
         // Green
-        println!("Green");
         red.off();
         amber.off();
         green.on();
         thread::sleep(time::Duration::from_secs(5));
 
         // Yellow
-        println!("Yellow");
         green.off();
         amber.on();
         thread::sleep(time::Duration::from_secs(2));
 
         // Yellow off
-        println!("Yellow off");
         amber.off();
     }
+
+    red.off();
+    amber.off();
+    green.off();
 }
